a97f92333a760d95400a9cb0c994fa7b485ac4af
madhursharma026 <105696243+madhursharma026@users.noreply.github.com>
2022-07-17 00:49:06 +0530

    Update URL

--- src/api.js
+++ src/api.js
@@ -5,7 +5,9 @@ import React, {useState, useEffect} from "react";
 
 
 
-export const API_URL ='http://localhost:8000'
+// export const API_URL ='http://localhost:8000'
+export const API_URL ='https://tfbp-backend.herokuapp.com/accounts/'
+
 export const api = axios.create({
   baseURL: API_URL,
   headers: {
